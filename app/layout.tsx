3699db4ec6fd2c5d8a836148927e7d64b7acf033
Sophia Alexandra <alexconner470@gmail.com>
2025-07-16 11:35:02 -0300

    feat(layout): added Metadata to improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,60 @@
 import {ReactNode} from 'react';
+import {Metadata} from 'next';
 
-// Metadata defined in app/[locale]/layout.tsx will be used.
-// If you have truly global, non-locale-specific metadata, it can be here.
-// export const metadata = {
-//   title: 'Global App Title',
-// };
+export const metadata: Metadata = {
+   title: 'IBVI | A FIPE do Mercado Imobiliário',
+   description:
+      'O IBVI utiliza análises avançadas, aprendizado de máquina e avaliações padronizadas para impulsionar a transparência, precisão e eficiência no mercado imobiliário brasileiro.',
+   keywords: 'IBVI, FIPE, mercado imobiliário, avaliação de imóveis, transparência imobiliária, precisão imobiliária, eficiência imobiliária',
+   applicationName: 'IBVI Luxury Real Estate',
+   authors: [{name: 'IBVI', url: 'https://www.ibvi.com.br'}],
+   generator: 'Next.js',
+   creator: 'IBVI',
+   publisher: 'IBVI',
+   formatDetection: {
+      email: false,
+      telephone: false,
+   },
+   metadataBase: new URL('https://www.ibvi.com.br'),
+   alternates: {
+      canonical: '/',
+   },
+   openGraph: {
+      type: 'website',
+      locale: 'pt_BR',
+      url: 'https://www.ibvi.com.br',
+      title: 'IBVI | A FIPE do Mercado Imobiliário',
+      description:
+         'O IBVI utiliza análises avançadas, aprendizado de máquina e avaliações padronizadas para impulsionar a transparência, precisão e eficiência no mercado imobiliário brasileiro.',
+      siteName: 'IBVI',
+      images: [
+         {
+            url: 'https://img.mbras.com.br/ibvi/ibvi.png',
+            alt: 'IBVI Logo',
+         },
+      ],
+   },
+   twitter: {
+      card: 'summary_large_image',
+      title: 'IBVI | A FIPE do Mercado Imobiliário',
+      description: 'O IBVI utiliza análises avançadas, aprendizado de máquina e avaliações padronizadas para impulsionar a transparência, precisão e eficiência no mercado imobiliário brasileiro.',
+      images: ['https://img.mbras.com.br/ibvi/ibvi.png'],
+   },
+   robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot: {
+         index: true,
+         follow: true,
+         'max-image-preview': 'large',
+         'max-snippet': -1,
+      },
+   },
+   verification: {
+      google: process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION,
+   },
+};
 
 export default function RootLayout({children}: {children: ReactNode}) {
    return children;
